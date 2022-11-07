4a34afeb891641ca6e44fd01f6785858448a4629
Luke Wilson <luke.wilson@intelligenthack.com>
2022-11-07 18:48:31 +0000

    Rename

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UploadView from '../views/UploadView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: UploadView
+      component: HomeView
     },
     {
       path: '/instantiate',
