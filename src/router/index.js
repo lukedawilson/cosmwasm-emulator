e1a102bb74a1c637cc74eb12ec8fc281c7abec50
Luke Wilson <luke.wilson@intelligenthack.com>
2022-11-16 14:05:34 +0000

    Lazy-load routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import InstantiateView from '../views/InstantiateView.vue'
-import EmulatorView from '../views/EmulatorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,12 +12,12 @@ const router = createRouter({
     {
       path: '/instantiate',
       name: 'instantiate',
-      component: InstantiateView
+      component: () => import('../views/InstantiateView.vue')
     },
     {
       path: '/emulator',
       name: 'emulator',
-      component: EmulatorView
+      component: () => import('../views/EmulatorView.vue')
     }
   ]
 })
