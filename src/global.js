0d6e9aac898b3bfb2aadd45a81522692b8e454ff
Luke Wilson <luke.wilson@intelligenthack.com>
2022-11-19 13:41:45 +0000

    Move funcs to global

--- src/global.js
+++ src/global.js
@@ -1,9 +1,74 @@
-export const sender = 'terra1hgm0p7khfk85zpz5v0j8wnej3a90w709vhkdfu'
-export const funds = []
+import state from './state/state'
+import { CWSimulateApp } from '@terran-one/cw-simulate'
+
+export const defaultAppConfig = {
+  chainId: 'phoenix-1',
+  bech32Prefix: 'terra'
+}
+
+const sender = 'terra1hgm0p7khfk85zpz5v0j8wnej3a90w709vhkdfu'
+const funds = []
+
+const missingFieldError = 'missing field'
+const invalidTypeError = 'Invalid type'
+const invalidWholeError = 'Error parsing whole'
+const invalidDigitError = 'invalid digit found in string'
 
 export const formatResult = result => {
   const response = result.ok
     ? { timestamp: new Date(Date.now()), ...result.val }
     : { timestamp: new Date(Date.now()), error: result.val }
   return JSON.stringify(response, null, 2)
+}
+
+export const doInstantiate = async (app, message) => {
+  const codeId = app.wasm.create(sender, state.wasmBytecode)
+  try {
+    return await app.wasm.instantiateContract(sender, funds, codeId, message)
+  } catch (e) {
+    return { val: e.message }
+  }
+}
+
+export const buildSchema = async() => {
+  const schema = {}
+  const typeVals = ["STRING", 0.0, 0, "0.0", "0"]
+  let typeIdx = 0, field
+
+  const dummyApp = new CWSimulateApp(defaultAppConfig)
+
+  while (true) {
+    const result = await doInstantiate(dummyApp, schema)
+    if (result.ok) {
+      return schema
+    }
+
+    let idx
+    if ((idx = result.val.indexOf(missingFieldError)) !== -1) {
+      typeIdx = 0
+
+      idx += missingFieldError.length
+      field = result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
+
+      schema[field] = typeVals[typeIdx]
+    } else if (result.val.indexOf(invalidTypeError) !== -1 || result.val.indexOf(invalidWholeError) !== -1 || result.val.indexOf(invalidDigitError) !== -1) {
+      if (typeIdx === typeVals.length - 1) {
+        schema[field] = "UNKNOWN_TYPE"
+        return schema
+      }
+
+      typeIdx ++
+      schema[field] = typeVals[typeIdx]
+    } else {
+      return schema
+    }
+  }
+}
+
+export const getMissingFieldName = (result) => {
+  let idx = result.val.indexOf(missingFieldError)
+  if (idx === -1) return null
+
+  idx += missingFieldError.length
+  return result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
 }
--- src/views/InstantiateView.vue
+++ src/views/InstantiateView.vue
@@ -26,69 +26,7 @@
   import { CWSimulateApp } from '@terran-one/cw-simulate'
   import JsonInput from '../components/JsonInput.vue'
   import state from '../state/state'
-  import { sender, funds, formatResult } from '../global'
-
-  const defaultAppConfig = {
-    chainId: 'phoenix-1',
-    bech32Prefix: 'terra'
-  }
-
-  const missingFieldError = 'missing field'
-  const invalidTypeError = 'Invalid type'
-  const invalidWholeError = 'Error parsing whole'
-  const invalidDigitError = 'invalid digit found in string'
-
-  async function doInstantiate(app, message) {
-    const codeId = app.wasm.create(sender, state.wasmBytecode)
-    try {
-      return await app.wasm.instantiateContract(sender, funds, codeId, message)
-    } catch (e) {
-      return { val: e.message }
-    }
-  }
-
-  async function buildSchema() {
-    const schema = {}
-    const typeVals = ["STRING", 0.0, 0, "0.0", "0"]
-    let typeIdx = 0, field
-
-    const dummyApp = new CWSimulateApp(defaultAppConfig)
-
-    while (true) {
-      const result = await doInstantiate(dummyApp, schema)
-      if (result.ok) {
-        return schema
-      }
-
-      let idx
-      if ((idx = result.val.indexOf(missingFieldError)) !== -1) {
-        typeIdx = 0
-
-        idx += missingFieldError.length
-        field = result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
-
-        schema[field] = typeVals[typeIdx]
-      } else if (result.val.indexOf(invalidTypeError) !== -1 || result.val.indexOf(invalidWholeError) !== -1 || result.val.indexOf(invalidDigitError) !== -1) {
-        if (typeIdx === typeVals.length - 1) {
-          schema[field] = "UNKNOWN_TYPE"
-          return schema
-        }
-
-        typeIdx ++
-        schema[field] = typeVals[typeIdx]
-      } else {
-        return schema
-      }
-    }
-  }
-
-  function getMissingFieldName(result) {
-    let idx = result.val.indexOf(missingFieldError)
-    if (idx === -1) return null
-
-    idx += missingFieldError.length
-    return result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
-  }
+  import { defaultAppConfig, doInstantiate, buildSchema, getMissingFieldName, formatResult } from '../global'
 
   export default {
     components: {
