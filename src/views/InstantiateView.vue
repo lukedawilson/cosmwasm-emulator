2a38bc9021b065a3e375db017ceb89320b6b8813
Luke Wilson <luke.wilson@intelligenthack.com>
2022-11-19 13:35:25 +0000

    Refactor

--- src/views/InstantiateView.vue
+++ src/views/InstantiateView.vue
@@ -28,19 +28,67 @@
   import state from '../state/state'
   import { sender, funds, formatResult } from '../global'
 
-  async function doInstantiate(message) {
-    const codeId = state.app.wasm.create(sender, state.wasmBytecode)
-    try {
-      return await state.app.wasm.instantiateContract(sender, funds, codeId, message)
-    } catch (e) {
-      return { val: e.message }
-    }
+  const defaultAppConfig = {
+    chainId: 'phoenix-1',
+    bech32Prefix: 'terra'
   }
 
   const missingFieldError = 'missing field'
   const invalidTypeError = 'Invalid type'
   const invalidWholeError = 'Error parsing whole'
   const invalidDigitError = 'invalid digit found in string'
+
+  async function doInstantiate(app, message) {
+    const codeId = app.wasm.create(sender, state.wasmBytecode)
+    try {
+      return await app.wasm.instantiateContract(sender, funds, codeId, message)
+    } catch (e) {
+      return { val: e.message }
+    }
+  }
+
+  async function buildSchema() {
+    const schema = {}
+    const typeVals = ["STRING", 0.0, 0, "0.0", "0"]
+    let typeIdx = 0, field
+
+    const dummyApp = new CWSimulateApp(defaultAppConfig)
+
+    while (true) {
+      const result = await doInstantiate(dummyApp, schema)
+      if (result.ok) {
+        return schema
+      }
+
+      let idx
+      if ((idx = result.val.indexOf(missingFieldError)) !== -1) {
+        typeIdx = 0
+
+        idx += missingFieldError.length
+        field = result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
+
+        schema[field] = typeVals[typeIdx]
+      } else if (result.val.indexOf(invalidTypeError) !== -1 || result.val.indexOf(invalidWholeError) !== -1 || result.val.indexOf(invalidDigitError) !== -1) {
+        if (typeIdx === typeVals.length - 1) {
+          schema[field] = "UNKNOWN_TYPE"
+          return schema
+        }
+
+        typeIdx ++
+        schema[field] = typeVals[typeIdx]
+      } else {
+        return schema
+      }
+    }
+  }
+
+  function getMissingFieldName(result) {
+    let idx = result.val.indexOf(missingFieldError)
+    if (idx === -1) return null
+
+    idx += missingFieldError.length
+    return result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
+  }
 
   export default {
     components: {
@@ -67,20 +115,15 @@
         }
       },
       async instantiate() {
-        state.app = new CWSimulateApp({
-          chainId: 'phoenix-1',
-          bech32Prefix: 'terra'
-        })
+        state.app = new CWSimulateApp(defaultAppConfig)
 
-        const result = await doInstantiate(this.message)
+        const result = await doInstantiate(state.app, this.message)
         if (!result.ok) {
           // if the error is 'missing field', add a placeholder for the missing field to the message
-          let idx = result.val.indexOf(missingFieldError)
-          if (idx !== -1) {
-            idx += missingFieldError.length
-            const field = result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
-            this.message[field] = `MY_${field.toLocaleUpperCase()}`
-            this.initialValue = JSON.stringify(this.message, null, 2).replace(`"${this.message[field]}"`, this.message[field])
+          let missingField = getMissingFieldName(result)
+          if (missingField) {
+            this.message[missingField] = `MY_${missingField.toLocaleUpperCase()}`
+            this.initialValue = JSON.stringify(this.message, null, 2).replace(`"${this.message[missingField]}"`, this.message[missingField])
           }
 
           this.isValid = false
@@ -99,7 +142,7 @@
         bech32Prefix: 'terra'
       })
 
-      let result = await doInstantiate(this.message)
+      let result = await doInstantiate(state.app, {})
       if (result.ok) {
         state.contractAddress = result.val.events[0].attributes[0].value
         this.$router.push('/emulator')
@@ -107,56 +150,10 @@
       }
 
       // build up schema from error messages
-      const schema = {}
-      const typeVals = ["STRING", 0.0, 0, "0.0", "0"]
-      let typeIdx = 0, field
-
-      while (true) {
-        result = await doInstantiate(schema)
-        if (result.ok) {
-          this.initialValue = JSON.stringify(schema, null, 2)
-          state.app = new CWSimulateApp({
-            chainId: 'phoenix-1',
-            bech32Prefix: 'terra'
-          })
-          this.message = schema
-          this.isValid = true
-          return
-        }
-
-        let idx
-        if ((idx = result.val.indexOf(missingFieldError)) !== -1) {
-          typeIdx = 0
-          idx += missingFieldError.length
-          field = result.val.slice(idx).split('`').map(x => x?.trim()).filter(x => !!x)[0]
-
-          schema[field] = typeVals[typeIdx]
-        } else if (result.val.indexOf(invalidTypeError) !== -1 || result.val.indexOf(invalidWholeError) !== -1 || result.val.indexOf(invalidDigitError) !== -1) {
-          if (typeIdx === typeVals.length - 1) {
-            schema[field] = "UNKNOWN_TYPE"
-            this.initialValue = JSON.stringify(schema, null, 2).replace('"UNKNOWN_TYPE"', "UNKNOWN_TYPE")
-            state.app = new CWSimulateApp({
-              chainId: 'phoenix-1',
-              bech32Prefix: 'terra'
-            })
-            this.message = schema
-            this.isValid = true
-            return
-          }
-
-          typeIdx ++
-          schema[field] = typeVals[typeIdx]
-        } else {
-          this.initialValue = JSON.stringify(schema, null, 2)
-          state.app = new CWSimulateApp({
-            chainId: 'phoenix-1',
-            bech32Prefix: 'terra'
-          })
-          this.message = schema
-          this.isValid = true
-          return
-        }
-      }
+      const schema = await buildSchema()
+      this.initialValue = JSON.stringify(schema, null, 2).replaceAll('"UNKNOWN_TYPE"', "UNKNOWN_TYPE")
+      this.message = schema
+      this.isValid = true
     }
   };
 </script>
