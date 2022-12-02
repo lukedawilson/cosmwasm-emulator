acf10d4b08d4bda528b968f54850a3359627daac
Luke Wilson <luke.wilson@intelligenthack.com>
2022-12-02 15:56:43 +0000

    Tidy up layout

--- src/views/EmulatorView.vue
+++ src/views/EmulatorView.vue
@@ -1,48 +1,46 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Execute</h5>
+  <div class="row">
+    <div class="col">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h5 class="card-title">Execute</h5>
 
-            <label>Message JSON:</label>
-            <JsonInput v-on:change="handleChange(this.execute, $event)" />
+          <label>Message JSON:</label>
+          <JsonInput v-on:change="handleChange(this.execute, $event)" />
 
-            <div class="d-md-flex text-center mt-4">
-              <button class="btn btn-primary" v-on:click="doExecute" :class="{ disabled: !execute.isValid }">Execute</button>
-            </div>
-
-            <label class="mt-4">Response:</label>
-            <textarea readonly class="form-control" rows="10" v-model="execute.response" :class="{ 'text-danger': !execute.isSuccess, 'text-success': execute.isSuccess }"></textarea>
+          <div class="d-md-flex text-center mt-4">
+            <button class="btn btn-primary" v-on:click="doExecute" :class="{ disabled: !execute.isValid }">Execute</button>
           </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Query</h5>
 
-            <label>Message JSON:</label>
-            <JsonInput v-on:change="handleChange(this.query, $event)" />
-
-            <div class="d-md-flex text-center mt-4">
-              <button class="btn btn-primary" v-on:click="doQuery" :class="{ disabled: !query.isValid }">Query</button>
-            </div>
-
-            <label class="mt-4">Response:</label>
-            <textarea readonly class="form-control" rows="10" v-model="query.response" :class="{ 'text-danger': !query.isSuccess, 'text-success': query.isSuccess }"></textarea>
-          </div>
+          <label class="mt-4">Response:</label>
+          <textarea readonly class="form-control" rows="6" v-model="execute.response" :class="{ 'text-danger': !execute.isSuccess, 'text-success': execute.isSuccess }"></textarea>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">State</h5>
-            <textarea readonly class="form-control" rows="10" v-model="simulationState"></textarea>
+    <div class="col">
+      <div class="card mb-4">
+        <div class="card-body">
+          <h5 class="card-title">Query</h5>
+
+          <label>Message JSON:</label>
+          <JsonInput v-on:change="handleChange(this.query, $event)" />
+
+          <div class="d-md-flex text-center mt-4">
+            <button class="btn btn-primary" v-on:click="doQuery" :class="{ disabled: !query.isValid }">Query</button>
           </div>
+
+          <label class="mt-4">Response:</label>
+          <textarea readonly class="form-control" rows="6" v-model="query.response" :class="{ 'text-danger': !query.isSuccess, 'text-success': query.isSuccess }"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">State</h5>
+          <textarea readonly class="form-control text-info" rows="6" v-model="simulationState"></textarea>
         </div>
       </div>
     </div>
